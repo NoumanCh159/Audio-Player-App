8ff1e75f98d5f694d9b210656d9c49a3d4addb81
Muhammad Nouman <19011519-159@uog.edu.pk>
2022-03-23 18:15:03 +0500

    made changes

--- audioplayer/src/Cmps/Card/Card.js
+++ audioplayer/src/Cmps/Card/Card.js
@@ -9,3 +9,35 @@ import voldnicon from './images/volumedn.png';
 import volupicon from './images/volumup.png';
 import { player } from '../script.js'
 import { volume } from '../script.js'
+
+function Card() {
+    return (
+        <div className="AudioTile" id="AudioTile">
+            <div className="AudioTitle">
+                <h3>Into Your Arms</h3>
+            </div>
+            <div className="AudioImage">
+                <img className="AudioImage" src={tileimage}/>
+                    <div className="AudioPlayer">
+                        <audio id="player" src={tilemp3} preload="metadata" type="audio/mpeg">
+                        </audio>
+                        <div className="AudioPlace">
+                            <div className="buttons">
+                                <button onClick={player}><img id="btnPlay" src={playicon} /></button>
+                                <div className="timer">
+                                    <span id="showtime">0:00 </span>/<span id="duration"></span>
+                                </div>
+                                <progress id="seek" value="0"></progress>
+                                <span id="BtnVol">
+                                    <button onClick={volume}><img id="btnVol" src={voldnicon} /></button>
+                                    <input type="range" id="volseek" value="0" />
+                                </span>
+                                <button id="movbtnSol"><img id="btnSol" src={moreicon} /></button>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    );
+}
+export default Card
