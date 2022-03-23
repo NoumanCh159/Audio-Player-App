b881e0f03369077a7be27e610d3c842d617c7fba
Muhammad Nouman <19011519-159@uog.edu.pk>
2022-03-23 17:47:27 +0500

    Added things in the app.

---
+++ audioplayer/src/Cmps/Card/Card.js
@@ -0,0 +1,11 @@
+import './Card.css';
+import './CardMQ.css';
+import tileimage from './images/hey.jpg';
+import tilemp3 from './images/hey.mp3';
+import moreicon from './images/more.png';
+import pauseicon from './images/pause.png';
+import playicon from './images/play.png';
+import voldnicon from './images/volumedn.png';
+import volupicon from './images/volumup.png';
+import { player } from '../script.js'
+import { volume } from '../script.js'
